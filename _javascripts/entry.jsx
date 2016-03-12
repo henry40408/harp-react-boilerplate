bc2eb4bc760517ea993264c705d204efe477efaa
Heng-Yi Wu <henry40408@gmail.com>
2016-03-12 17:42:21 +0800

    enable hot module replacement

--- _javascripts/entry.jsx
+++ _javascripts/entry.jsx
@@ -6,7 +6,7 @@ console.log("Application initialized.");
 class App extends React.Component {
     render() {
         return (<div>
-            <h1>Welcome to Harp.</h1>
+            <h1>Welcome to Harp.foobar</h1>
             <h3>This is yours to own. Enjoy.</h3>
         </div>);
     }
